ad0f30acb0fb980ef7b9ec6c72c0336fcc923a68
LMezza <mezabalza18@gmail.com>
2024-11-20 09:54:25 +0000

    generator of domains

---
+++ app.js
@@ -0,0 +1,20 @@
+let pronoun = ['the', 'our', 'my'];
+let adj = ['great', 'big', 'small', 'boring'];
+let noun = ['jogger', 'raccon'];
+let domain = ['.com', '.org', '.net', '.info']
+
+
+
+for (let n = 0; n < pronoun.length; n++) {
+    for (let i = 0; i < adj.length; i++) {
+        for (let pr = 0; pr < noun.length; pr++) {
+            for (let index = 0; index < domain.length; index++) {
+                if (domain[index] === '.com') {
+                    console.log(pronoun[n] + adj[i] + noun[pr] +'.es');
+                } else {
+                    console.log (pronoun[n] + adj[i] + noun[pr] + domain[index]);
+                }
+            }
+        }
+    }
+}
